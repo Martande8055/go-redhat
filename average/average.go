3c80fd6ab6b0ca2a0612714f030f40272bc24471
Saurabh Martande <smartand@smartand-thinkpadt14sgen2i.punetw6.csb>
2024-10-07 14:34:32 +0530

    first commit

---
+++ average/average.go
@@ -0,0 +1,29 @@
+package average
+
+import (
+	"fmt"
+	"os"
+)
+
+// CalculateAverage reads float64 numbers from standard input,
+// calculates the average, and returns it.
+func CalculateAverage() (float64, error) {
+	var n int
+	var sum float64
+
+	for {
+    	var val float64
+    	_, err := fmt.Fscan(os.Stdin, &val)
+    	if err != nil {
+        	break
+    	}
+    	sum += val
+    	n++
+	}
+
+	if n == 0 {
+    	return 0, fmt.Errorf("no numbers provided")
+	}
+
+	return sum / float64(n), nil
+}
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"module_name/average" // replace `your_module_name` with the actual module name
+)
+
+func main() {
+	// avg, err := average.CalculateAverage()
+	// if err != nil {
+    // 	log.Println(err)
+	// } else {
+    // 	fmt.Printf("The average is %.2f\n", avg)
+	// }
+
+	closer();
+}
