0e2bbe950d88e3b0e2eaf0948133b46263396a5c
Saurabh Martande <smartand@smartand-thinkpadt14sgen2i.punetw6.csb>
2024-10-08 14:39:23 +0530

    added test cases

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"RelevantDarkUpgrade/average"
+	"GO_PROJECT/average"
 	"fmt"
 	"log"
 )
@@ -15,18 +15,18 @@ func print(val float64, err error) {
 
 func main() {
 
-	even := &average.Even{Values: []float64{1, 3, 4.2, 6, 8, 10}}
-	odd := &average.Odd{Values: []float64{1, 4, 3,7, 6, 8, 10}}
-	evenodd := &average.EvenOdd{Values: []float64{1, 3, 2.1, 4, 6, 8, 10}}
+	var even average.Avg = &average.Even{Values: []float64{1, 3, 4.2, 6, 8, 10}}
+	var odd average.Avg = &average.Odd{Values: []float64{1, 4, 3, 7, 6.1, 8, 10}}
+	var evenodd average.Avg = &average.EvenOdd{Values: []float64{1, 3, 2.1, 4, 6, 8, 10}}
 
 	fmt.Print("even= ")
 	print(even.CalcuateAverage())
-	fmt.Println("count of even no=",even.Num)
+	fmt.Println("count of even no=", even.(*average.Even).Num)
 	fmt.Print("odd= ")
 	print(odd.CalcuateAverage())
-	fmt.Println("count of odd no=",odd.Num)
+	fmt.Println("count of odd no=", odd.(*average.Odd).Num)
 	fmt.Print("EvenOdd= ")
 	print(evenodd.CalcuateAverage())
-	fmt.Println("count of numbers=",evenodd.Num)
+	fmt.Println("count of numbers=", evenodd.(*average.EvenOdd).Num)
 
 }
