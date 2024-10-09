cdd7e75fee1717974d0bc9234ad008dcd1cd8613
Saurabh Martande <smartand@smartand-thinkpadt14sgen2i.punetw6.csb>
2024-10-09 19:58:00 +0530

    removed default calls

--- main.go
+++ main.go
@@ -38,16 +38,6 @@ func bitArrayTofloat(b []byte) ([]float64, error) {
 
 func main() {
 
-	var odd average.Avg = &average.Odd{Values: []float64{1, 4, 3, 7, 6.1, 8, 10}}
-	var evenodd average.Avg = &average.EvenOdd{Values: []float64{1, 3, 2.1, 4, 6, 8, 10}}
-
-	fmt.Print("odd= ")
-	print(odd.CalcuateAverage())
-	fmt.Println("count of odd no=", odd.(*average.Odd).Num)
-	fmt.Print("EvenOdd= ")
-	print(evenodd.CalcuateAverage())
-	fmt.Println("count of numbers=", evenodd.(*average.EvenOdd).Num)
-
 	http.HandleFunc("/even", evenAvgHandler)
 	http.HandleFunc("/odd", oddAvgHandler)
 	http.HandleFunc("/evenodd", evenoddAvgHandler)
