016e684c973176cb2ae78a4c2216971813f32504
Saurabh Subhash Martande <smartande8055@gmail.com>
2024-10-14 01:45:25 +0530

    feat: added new operations and structure modified (#3)
    Co-authored-by: Saurabh Martande <smartand@smartand-thinkpadt14sgen2i.punetw6.csb>

--- main.go
+++ main.go
@@ -1,140 +1,17 @@
 package main
 
 import (
-	"GO_PROJECT/average"
-	"fmt"
-	"io"
+	"GO_PROJECT/handler"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
 )
-
-func print(val float64, err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(val)
-}
-
-func bitArrayTofloat(b []byte) ([]float64, error) {
-	data := string(b)
-
-	string_Numbers := strings.Split(data, ",")
-
-	var nums []float64
-
-	for _, str := range string_Numbers {
-		str = strings.TrimSpace(str)
-		if num, err := strconv.ParseFloat(str, 64); err != nil {
-
-			return []float64{}, err
-		} else {
-			nums = append(nums, num)
-		}
-	}
-	return nums, nil
-}
 
 func main() {
 
-	http.HandleFunc("/even", evenAvgHandler)
-	http.HandleFunc("/odd", oddAvgHandler)
-	http.HandleFunc("/evenodd", evenoddAvgHandler)
+	http.HandleFunc("/even", handler.EvenAvg)
+	http.HandleFunc("/odd", handler.OddAvg)
+	http.HandleFunc("/evenodd", handler.EvenoddAvg)
 	log.Println("server started")
 	log.Fatal(http.ListenAndServe(":3100", nil))
-
-}
-
-func evenAvgHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "failed to read req body", http.StatusInternalServerError)
-		return
-	}
-
-	nums, err := bitArrayTofloat(body)
-	if err != nil {
-		http.Error(w, "invalid input", http.StatusNotAcceptable)
-		return
-	}
-
-	var even average.Avg = &average.Even{Values: nums}
-
-	avg, err := even.CalcuateAverage()
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "Avg of even no is %.2f \n", avg)
-
-}
-
-func oddAvgHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "failed to read req body", http.StatusInternalServerError)
-		return
-	}
-
-	nums, err := bitArrayTofloat(body)
-	if err != nil {
-		http.Error(w, "invalid input", http.StatusNotAcceptable)
-		return
-	}
-
-	var odd average.Avg = &average.Odd{Values: nums}
-
-	avg, err := odd.CalcuateAverage()
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "Avg of even no is %.2f \n", avg)
-
-}
-
-func evenoddAvgHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "failed to read req body", http.StatusInternalServerError)
-		return
-	}
-
-	nums, err := bitArrayTofloat(body)
-	if err != nil {
-		http.Error(w, "invalid input", http.StatusNotAcceptable)
-		return
-	}
-
-	var evenodd average.Avg = &average.EvenOdd{Values: nums}
-
-	avg, err := evenodd.CalcuateAverage()
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "Avg of even no is %.2f \n", avg)
 
 }
